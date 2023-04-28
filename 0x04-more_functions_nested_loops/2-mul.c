e65af32bde20938ccc9a00d0831e22293618ac6d
Manar Elsaid <elsaidmanar@gmail.com>
2023-04-28 14:10:04 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first input of the function
+ * @b: seconed input of the function
+ * Return: always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	float mul;
+
+	mul = a * b;
+}
+
