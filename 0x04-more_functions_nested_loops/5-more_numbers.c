f5b6fad9aea29e3588450f3f82b97340eb6f6479
Manar Elsaid <elsaidmanar@gmail.com>
2023-04-28 16:11:00 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,15 @@
 
 void more_numbers(void)
 {
-	char ch;
+	char ch, c;
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (ch = '0'; ch <= '14'; ch++)
+		for (ch = '0'; ch <= '9'; ch++)
 			_putchar(ch);
+		for (c = '0'; c <= '4'; c++)
+			_putchar('1' + 'c');
 		_putchar("\n");
 	}
 }
