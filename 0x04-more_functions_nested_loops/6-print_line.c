eac140969e48b94f7b53049c5eed975f28c3a6e9
Manar Elsaid <elsaidmanar@gmail.com>
2023-04-28 16:50:12 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,11 @@ void print_line(int n)
 	int count;
 
 	if (n <= 0)
-		_putchar("\n");
+		_putchar('\n');
 	else
 		for (count = 0; count < n; count++)
 		{
 			_putchar('_');
 		}
-	_putchar("\n");
+	_putchar('\n');
 }
