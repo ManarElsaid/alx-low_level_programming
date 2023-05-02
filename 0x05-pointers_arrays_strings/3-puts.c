575277b6e56882efe5b2471aa91827549360efe3
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-02 09:49:23 -0700

    print string followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string's pointer
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; str++)
+	{
+		_putchar(*str);
+		_putchar("\n");
+		i++;
+	}
+}
