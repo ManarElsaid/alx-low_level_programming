730a8fd86940d3da7f90d8dbc993a2dc2779ef1b
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-02 16:30:16 -0700

    prints half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,13 @@ void puts_half(char *str)
 		len++;
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i++; i <= len - 1)
+		for (i = len / 2; i <= len - 1; i++)
 			_putchar(str[i]);
 	}
 
 	else if (len % 2 == 1)
 	{
-		for (i = (len + 1) / 2; i++; i <= len - 1)
+		for (i = (len + 1) / 2; i <= len - 1; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
