fa22fd1d776863bea57418e12151ce2e9783decc
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-02 16:27:04 -0700

    prints half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of string
+ * @str: input of the function
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i++; i <= len - 1)
+			_putchar(str[i]);
+	}
+
+	else if (len % 2 == 1)
+	{
+		for (i = (len + 1) / 2; i++; i <= len - 1)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
