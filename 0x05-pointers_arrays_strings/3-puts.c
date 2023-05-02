5434aef945882b63fa5e307a281b31aa55d13ead
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-02 09:59:40 -0700

    print string followed by new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,10 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; *str != '\0'; str++)
+	for (i = 0; *str != '\0'; *str++)
 	{
-		_putchar('*str');
-		_putchar("\n");
-		i++;
+		_putchar(*str);
+		i++
 	}
+	_putchar("\n");
 }
