87fc763f81d303dbca148944b308604ce81edb12
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-02 15:39:29 -0700

    prints every other char. of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the pointer of thestring
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	int len =
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of string
+ * @str: the pointer of a string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		_putchar(*str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
