75fe6da1e7a57048c43388ed3e09b6b2ddee3cdc
Manar Elsaid <elsaidmanar@gmail.com>
2023-05-04 17:51:03 -0700

    reversed array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ reverse_array(int *a, int n)
 {
 	int i, temp;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n--; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
+		a[i] = a[n];
+		a[n] = a[i];
 	}
 }
