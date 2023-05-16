f554c2aaad8fdb898394af01f5bd848684f1dfb5
ManarElsaid <elsaidmanar@gmail.com>
2023-05-16 16:58:28 -0700

    strdup

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,8 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	s = malloc(((strlen(s1) + strlen(s2)) + 1) * sizeof(char));
+	if (s == 0)
+		return (NULL);
 	if (s == NULL)
 		return (NULL);
 
