62fc315d0cf5c841855270788595eab4706c7509
ManarElsaid <elsaidmanar@gmail.com>
2023-06-05 04:51:33 -0700

    trial|

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -31,7 +31,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", _strlen(h->str), h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
