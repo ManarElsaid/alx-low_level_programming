e2fd2908b825a4b40fb216febbccd38f699bf3e5
ManarElsaid <elsaidmanar@gmail.com>
2023-06-06 03:14:14 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,14 +9,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	temp = head;
-
-	while (temp != NULL)
+	while (head != NULL)
 	{
-		temp = temp->next;
-		free(head);
-		head = temp;
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
-	head = NULL;
 }
 
