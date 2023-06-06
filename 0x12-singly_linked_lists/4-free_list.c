b70bd41c050bcc842791c7bbb49a13da23883d10
ManarElsaid <elsaidmanar@gmail.com>
2023-06-06 03:02:25 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - delets the list
+ * @head: pointer to the first node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = malloc(sizeof(list_t));
+
+	temp = head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
+
