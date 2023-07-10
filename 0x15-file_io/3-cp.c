ce748f636863e1bfc7db0e006140cc88884d9d9b
ManarElsaid <elsaidmanar@gmail.com>
2023-07-10 12:23:34 -0700

    main.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,27 @@
 #include "main.h"
 
+void error_file(int file_from, int file_to, char *argv[]);
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+        if (file_from == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+                exit(98);
+        }
+        if (file_to == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+                exit(99);
+        }
+}
+
 /**
  * main - copies the content of the file and displays error messeges
  * @argc: number of arguments.
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,26 +9,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_file(int file_from, int file_to, char *argv[]);
 
-/**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- */
-void error_file(int file_from, int file_to, char *argv[])
-{
-        if (file_from == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-                exit(98);
-        }
-        if (file_to == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                exit(99);
-        }
-}
 
 #endif
