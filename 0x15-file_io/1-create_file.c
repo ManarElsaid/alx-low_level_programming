1fa92ee1573e40f8b34a1e4be63dc88b7ce934c8
ManarElsaid <elsaidmanar@gmail.com>
2023-07-10 11:24:34 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to create
+ * @text_content: the string to write to the file 
+ * Return: 1 on success & 0 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n, rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (n = 0; text_content[nletters]; n++)
+		;
+
+	rwr = write(fd, text_content, n);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
