badbc021f5e2727d3cb5d7aee74df4c45ad63b22
ManarElsaid <elsaidmanar@gmail.com>
2023-07-10 11:28:07 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file.
  * @filename: the name of the file to create
- * @text_content: the string to write to the file 
+ * @text_content: the string to write to the file
  * Return: 1 on success & 0 on failure
  */
 int create_file(const char *filename, char *text_content)
