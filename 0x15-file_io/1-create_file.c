1cb53c4ee3ebeff0b60d3e664338798df09dc24f
ManarElsaid <elsaidmanar@gmail.com>
2023-07-10 11:26:49 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (n = 0; text_content[nletters]; n++)
+	for (n = 0; text_content[n]; n++)
 		;
 
 	rwr = write(fd, text_content, n);
