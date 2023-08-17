fa252d60e632c0ae3a552f997c7b1c57c70053dc
ManarElsaid <elsaidmanar@gmail.com>
2023-08-17 04:19:45 -0700

    README file

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * add_dnodeint -  adds a new node at the beginning of a dlistint_t list.
+ * @head: the head of the list
+ * @n: the data of the node
+ * Return:  the address of the new element, or NULL if it failed
+ */
+
