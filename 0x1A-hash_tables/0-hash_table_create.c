c2643fd175c5e421deab534d48d838903d298810
ManarElsaid <elsaidmanar@gmail.com>
2023-09-10 11:04:19 -0700

    correct the size of table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,15 +5,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc((table->size) * sizeof(hash_table_t));
+	table->array = malloc(sizeof(hash_node_t*) * table->size);
+
 	if (table->array == NULL)
 		return (NULL);
 
