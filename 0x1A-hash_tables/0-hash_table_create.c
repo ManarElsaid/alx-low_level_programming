73a136abae940dbd0df438c6559d6bc75e6e6954
ManarElsaid <elsaidmanar@gmail.com>
2023-09-10 10:37:38 -0700

    edit code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 	{
