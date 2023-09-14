346808b4a77ad0a39fd2b7819e81c7c91fdd39c6
ManarElsaid <elsaidmanar@gmail.com>
2023-09-13 17:32:04 -0700

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (count > 0)
 				printf(", ");
-			printf("%s: %s", node->key, node->value);
+			printf("\'%s\': \'%s\'", node->key, node->value);
 			count++;
 			node = node->next;
 		}
