d5536a614c1eb6eaa37c330b6423dae88c096e27
ManarElsaid <elsaidmanar@gmail.com>
2023-12-16 04:24:34 -0800

    103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -70,7 +70,7 @@ int exponential_search(int *array, size_t size, int value)
 		return (-1);
 
 	step = 1;
-	while (array[step] < value && step < size)
+	while (step < size && array[step] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", step, array[step]);
 		step = step * 2;
